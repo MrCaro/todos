3764630e5c1d25ba6cda6ae664a5a38c948ba438
MrCaro <36524539+MrCaro@users.noreply.github.com>
2023-06-13 18:55:45 -0400

    feat: prettier console log

--- src/app/api/server.tsx
+++ src/app/api/server.tsx
@@ -1,9 +1,10 @@
 "use server"
 
 import { prisma } from "@/db";
+import { color, log, red, green, cyan, cyanBright } from 'console-log-colors';
 
 export async function toggleTodo(id: string, complete: boolean) {
   
-  console.log('updating entry', id, ' value ', complete)
+  console.log(green(`updating entry => ' ${id}`))
   await prisma.todo.update({ where: { id }, data: { complete } })
 } 
