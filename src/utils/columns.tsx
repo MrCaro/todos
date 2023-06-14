319984b654f362fb837c52911ad53151f08c1a09
MrCaro <36524539+MrCaro@users.noreply.github.com>
2023-06-14 19:24:19 -0400

    feat: refresh button

--- src/utils/columns.tsx
+++ src/utils/columns.tsx
@@ -20,12 +20,22 @@ function refreshPage() {
 function onCheck(value: any, row: any) {
   row.toggleSelected(!!value)
   toggleTodo(row.original.id, !!value)
-  refreshPage()
+  // refreshPage()
 }
 
 export const columns: ColumnDef<Todos>[] = [
   {
     id: "select",
+    header: () => (
+      <button className="text-left flex gap-2" onClick={refreshPage}>
+        <p>
+          Refresh 
+        </p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+        </svg>
+      </button>
+    ),
     cell: ({ row }) => (
       <Checkbox
         defaultChecked={row.original.complete}
